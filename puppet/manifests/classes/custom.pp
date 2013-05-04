22cceda9cd4add37ed7828d4a1cc3c92a0dda548
Gavin Noronha <gavinln@hotmail.com>
2013-05-04 13:17:06 -0700

    Added script to download selenium jar

--- puppet/manifests/classes/custom.pp
+++ puppet/manifests/classes/custom.pp
@@ -1,9 +1,16 @@
 # You can add custom puppet manifests for your app here.
 class custom {
-    $selenium_jar = "selenium-server-standalone-2.32.0.jar"
-    $selenium_url = "http://selenium.googlecode.com/files/$selenium_jar"
+    $SELENIUM_JAR = "selenium-server-standalone-2.32.0.jar"
+    $SELENIUM_URL = "http://selenium.googlecode.com/files/$SELENIUM_JAR"
     exec { "download_selenium_standalone":
-        command => "curl -o $HOME_DIR/$selenium_jar $selenium_url",
-        unless  => "test -f $HOME_DIR/$selenium_jar",
+        command => "curl -o $PROJ_DIR/vm/selenium/$SELENIUM_JAR $SELENIUM_URL",
+        unless  => "test -f $PROJ_DIR/vm/selenium/$SELENIUM_JAR",
     }
+#	No need to create a script as one will be saved in git
+#    file { "$HOME_DIR/start_hub.sh":
+#        content => "#!/bin/bash\n\njava -jar $SELENIUM_JAR -role hub\n",
+#        ensure => "present",
+#        mode => "0755",
+#        require => Exec["download_selenium_standalone"],
+#    }
 }
